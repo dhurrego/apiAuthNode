d951527e9d79c0c3106305c713de76c664c1f87c
Deivid Urrego <deivid_199615@hotmail.com>
2021-05-18 17:09:11 -0500

    Separar el controlador de las rutas

---
+++ controllers/auth.js
@@ -0,0 +1,30 @@
+const { response } = require('express');
+const { request } = require('express');
+
+
+const crearUsuario = (req = request, res = response) => {
+    return res.json({
+        ok: true,
+        msg: 'Crear usuario /new'
+    });
+};
+
+const loginUsuario = (req = request, res = response) => {
+    return res.json({
+        ok: true,
+        msg: 'Login de usuario /'
+    });
+};
+
+const renewToken = (req = request, res = response) => {
+    return res.json({
+        ok: true,
+        msg: 'Renew token /renew'
+    });
+};
+
+module.exports = {
+    crearUsuario,
+    loginUsuario,
+    renewToken
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,16 @@
 const { Router } = require('express');
+const { crearUsuario, loginUsuario, renewToken } = require('../controllers/auth');
 
 const router = Router();
 
 // Crear un nuevo usuario
-router.post( '/new', (req, res) => {
-    return res.json({
-        ok: true,
-        msg: 'Crear usuario /new'
-    });
-});
+router.post( '/new', crearUsuario );
 
 // Login de usuario
-router.post( '/', (req, res) => {
-    return res.json({
-        ok: true,
-        msg: 'Login de usuario /'
-    });
-});
+router.post( '/', loginUsuario);
 
 // Validar y revalidar token
-router.get( '/renew', (req, res) => {
-    return res.json({
-        ok: true,
-        msg: 'Renew token /renew'
-    });
-});
+router.get( '/renew', renewToken );
 
 
 module.exports = router;
