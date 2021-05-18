67cec793e9a4db34f95fdc39d548f44ee5253b9d
Deivid Urrego <deivid_199615@hotmail.com>
2021-05-18 17:53:01 -0500

    Configurar CORS y body de las peticiones

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,8 @@ const { request } = require('express');
 
 
 const crearUsuario = (req = request, res = response) => {
+    const { email, name, password } = req.body;
+    console.log(email, name, password);
     return res.json({
         ok: true,
         msg: 'Crear usuario /new'
@@ -10,6 +12,8 @@ const crearUsuario = (req = request, res = response) => {
 };
 
 const loginUsuario = (req = request, res = response) => {
+    const { email, password } = req.body;
+    console.log(email, password);
     return res.json({
         ok: true,
         msg: 'Login de usuario /'
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 
 // Crear el servidor/aplicación de express
 const app = express();
 
-// Rutas
+// CORS
+app.use( cors() );
 
+// Lectura y parseo del body
+app.use( express.json() );
+
+// Rutas
 app.use( '/api/auth', require('./routes/auth') );
 
 // Ejecutar la aplicación
