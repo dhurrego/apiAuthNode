60678b253f6024c40c348a2f93df8676c4c62efd
Deivid Urrego <deivid_199615@hotmail.com>
2021-05-18 15:36:12 -0500

    Configuración de express

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hola desde Node');
+const express = require('express');
+
+// Crear el servidor/aplicación de express
+const app = express();
+
+// GET
+app.get('/', (req, res) => {
+    res.status(200).json({
+        ok: true,
+        msg: "Todo salio bien",
+        uid: 1234
+    });
+});
+
+// Ejecutar la aplicación
+app.listen( 4000, () => {
+    console.log(`Servidor corriendo en puerto ${ 4000 }`);
+});
