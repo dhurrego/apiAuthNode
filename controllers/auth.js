091d1f420fd6d59cc225c0900ce4a288de775532
Deivid Urrego <deivid_199615@hotmail.com>
2021-05-19 00:00:19 -0500

    Creación de usuario en BD

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,49 @@
 const { response } = require('express');
 const { request } = require('express');
+const Usuario = require('../models/Usuario');
 
-
-const crearUsuario = (req = request, res = response) => {
+const crearUsuario = async(req = request, res = response) => {
     const { email, name, password } = req.body;
-    console.log(email, name, password);
-    return res.json({
-        ok: true,
-        msg: 'Crear usuario /new'
-    });
+
+    try {
+        // Verificar email (que no exista)
+        const usuario = await Usuario.findOne({ email });
+
+        if(usuario){
+            return res.status(400).json({
+                ok:false,
+                msg: 'El usuario ya existe con ese email'
+            });
+        }
+
+        // Crear usuario con el modelo
+        const dbUser = new Usuario( req.body );
+
+        // Hash de la contraseña
+
+        // Generar el JWT
+
+        // Crear usuario de BD
+        await dbUser.save();
+
+        // Generar respuesta exitosa    
+        return res.status(201).json({
+            ok: true,
+            uid: dbUser.id,
+            name
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });    
+    }
+    
 };
 
 const loginUsuario = (req = request, res = response) => {
     const { email, password } = req.body;
-    console.log(email, password);
     return res.json({
         ok: true,
         msg: 'Login de usuario /'
