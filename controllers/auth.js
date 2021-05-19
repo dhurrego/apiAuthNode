48c1f9b6360c39d561affdfa7e8f604f2d2ae911
Deivid Urrego <deivid_199615@hotmail.com>
2021-05-19 00:27:23 -0500

    Login de usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -47,12 +47,49 @@ const crearUsuario = async(req = request, res = response) => {
     
 };
 
-const loginUsuario = (req = request, res = response) => {
+const loginUsuario = async(req = request, res = response) => {
     const { email, password } = req.body;
-    return res.json({
-        ok: true,
-        msg: 'Login de usuario /'
-    });
+
+    try {
+
+        const dbUser = await Usuario.findOne({ email });
+        
+        // Confirmar si el email existe
+        if(!dbUser){
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo o contraseña no son validos'
+            });
+        }
+
+        // Confirmar si el password es correcto
+        const validPassword = bcrypt.compareSync( password, dbUser.password);
+
+        if( !validPassword ){
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo o contraseña no son validos'
+            });
+        }
+
+        // Generar JWT
+        const token = await generarJWT(dbUser.id, dbUser.name);
+
+        // Generar respuesta exitosa    
+        return res.status(200).json({
+            ok: true,
+            uid: dbUser.id,
+            name: dbUser.name,
+            token
+        });
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Comuniquese con el administrador'
+        })
+    }
 };
 
 const renewToken = (req = request, res = response) => {
