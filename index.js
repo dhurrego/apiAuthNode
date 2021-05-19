617ba2b26801585b1d5b634581194ccf0fc47630
Deivid Urrego <deivid_199615@hotmail.com>
2021-05-18 23:45:07 -0500

    Conectar MongoDB Atlas - Compass y Node

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('./db/config');
 require('dotenv').config();
 
 // Crear el servidor/aplicación de express
 const app = express();
+
+// Conexión a la BD
+
+dbConnection();
 
 // Directorio público
 app.use( express.static('public'));
