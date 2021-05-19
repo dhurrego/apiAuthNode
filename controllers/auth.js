5f1b6c057acdff3933823001b1407a6119316d1e
Deivid Urrego <deivid_199615@hotmail.com>
2021-05-18 23:23:14 -0500

    Custom Middleware - ValidarCampos

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,8 @@
 const { response } = require('express');
 const { request } = require('express');
-const { validationResult } = require('express-validator');
 
 
 const crearUsuario = (req = request, res = response) => {
-    const errors = validationResult( req );
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }
     const { email, name, password } = req.body;
     console.log(email, name, password);
     return res.json({
@@ -20,13 +12,6 @@ const crearUsuario = (req = request, res = response) => {
 };
 
 const loginUsuario = (req = request, res = response) => {
-    const errors = validationResult( req );
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }
     const { email, password } = req.body;
     console.log(email, password);
     return res.json({
